7a768a09c33b37942e61cbc2bdaa5638f6a8497a
Jakir Hossain <jakirseu@gmail.com>
2023-09-21 03:17:50 +0600

    Initial Commit

---
+++ Tip Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tip Calculator
+//
+//  Created by Jakir Hossain on 21/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tip Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip Calculator
+//
+//  Created by Jakir Hossain on 21/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
